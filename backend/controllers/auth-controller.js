4eef0117e2bcb0ed5b3ef09cabf8686a52fad18f
Vivek9873 <103317330+Vivek9873@users.noreply.github.com>
2025-09-02 13:10:33 +0530

    Update auth-controller.js

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -31,8 +31,8 @@ export const signupUser = async (req, res) => {
         const token = await generateToken(newUser._id);
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "Strict",
+            secure: true,
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000,
         })
         return res.status(201).json({
@@ -74,8 +74,8 @@ export const loginUser = async (req, res) => {
         const token = await generateToken(findUser._id);
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "Strict",
+            secure: true,
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000,
         })
         return res.status(200).json({
@@ -182,8 +182,8 @@ export const googleSignUp = async (req, res) => {
         const token = await generateToken(user._id);
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "Strict",
+            secure: true,
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000,
         })
         return res.status(200).json({
